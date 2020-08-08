b59dd010a9c76231ec48f159927b4702e9ec5d3f
kaushal8707 <kaushal.singh029@gmail.com>
2020-08-08 16:59:52 +0530

    Initial commit

---
+++ src/test/java/com/always/learner/eurekaDiscoveryServerDemo/EurekaDiscoveryServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.always.learner.eurekaDiscoveryServerDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
